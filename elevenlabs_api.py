c59fb2f6b88c69ef18407b4702136c09bca21140
NGHTHWK <nghthwk@NGHTHWKs-MacBook-Pro.local>
2023-11-21 13:19:34 -0700

    Added rough API Code

--- elevenlabs_api.py
+++ elevenlabs_api.py
@@ -3,11 +3,47 @@ import requests
 ELEVENLABS_API_KEY = 'your_api_key_here'
 
 def create_voice(audio_file_path):
-    # Implement API call to upload audio and create voice
-    # Return voice ID
-    pass
+    # Read audio file contents
+    with open(audio_file_path, 'rb') as audio_file:
+        audio_data = audio_file.read()
+
+    # Prepare API request headers
+    headers = {
+        'Authorization': f'Bearer {ELEVENLABS_API_KEY}',
+        'Content-Type': 'application/octet-stream',
+    }
+
+    # Send API POST request to create voice
+    response = requests.post('https://api.elevenlabs.io/v2/voices', headers=headers, data=audio_data)
+
+    # Check for successful response
+    if response.status_code == 201:
+        # Extract voice ID from response JSON
+        voice_id = response.json()['id']
+        return voice_id
+    else:
+        raise Exception(f'Voice creation failed: {response.status_code} - {response.text}')
 
 def synthesize_speech(voice_id, text):
-    # Implement API call to synthesize speech using the created voice
-    # Return URL or path of the synthesized speech audio file
-    pass
+    # Prepare API request body
+    request_body = {
+        'voice_id': voice_id,
+        'text': text,
+    }
+
+    # Prepare API request headers
+    headers = {
+        'Authorization': f'Bearer {ELEVENLABS_API_KEY}',
+        'Content-Type': 'application/json',
+    }
+
+    # Send API POST request to synthesize speech
+    response = requests.post('https://api.elevenlabs.io/v2/audio', headers=headers, json=request_body)
+
+    # Check for successful response
+    if response.status_code == 201:
+        # Extract audio URL from response JSON
+        audio_url = response.json()['audio_url']
+        return audio_url
+    else:
+        raise Exception(f'Speech synthesis failed: {response.status_code} - {response.text}')
